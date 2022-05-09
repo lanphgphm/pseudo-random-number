0cfa3427d136cac79149b8c3788d8e21d2503388
LanPhuong <phuong.pham.210120@student.fulbright.edu.vn>
2022-05-09 22:37:49 +0700

    add docstrings

--- kotta.py
+++ kotta.py
@@ -1,50 +1,63 @@
+'''
+this program contains two functions: 
+    fibonacci(n):   generate fibonacci number at index n (helper function)
+    kotta():        the kotta pseudo-random number algorithm (main function)
+'''
 from cmath import sqrt
 import random 
 
 # step 3 
-def fibonacci(n: int): 
-    # FIBONACCI SEQUENCE GENERATED IS A LOT LONGER THAN REQUIRED 
-    f1 = 0
-    f2 = 1
-    f = []
-    for i in range(0, n+1): 
-        if i == 1: 
-            f.append(f1)
-        if i == 2: 
-            f.append(f2)
-        next = f1 + f2
-        f1 = f2
-        f2 = next
-        f.append(next)
-    return f
+def fibonacci(n: int) -> int:  
+    '''
+    returns fibonacci number at index (n - 1), start counting from 0
+
+    parameter: 
+        n: index of the fibonacci number 
+    
+    outputs: 
+        fibonacci number at index n 
+    '''
+    if n == 0: 
+        return 0
+    elif n == 1: 
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 def kotta():
+    '''
+    the kotta algorithm to generate pseudo-random number  
+
+    parameters: 
+        none
+    
+    outputs: 
+        random-looking sequence, whose size is the input value n from user 
+    '''
     # step 1
     a = 5 
     c = 1
-    m = 2 ** 32
+    m = 2**3 # when done use m = 2 ** 32 or m = 2 ** 64 instead
     # step 2  
     n = eval(input("Enter sample size: "))
     # step 3 
-    f = fibonacci(n)
+    f = [fibonacci(i) for i in range(n)]
     print(f"Fibonacci sequence: {f}")
-    x = [0] * n 
     # step 4
-    x[0] = random.random()
+    x = [0] * n
+    x[0] = random.randint(0, n + 1) # WHAT SHOULD BE THE RANGE OF RANDINT? 
     # step 5
-    for i in range(0, n - 1):
+    for i in range(1, n - 1):
         x[i + 1] = (a * x[i] + c + int(f[i]/x[0])) % m 
     # step 6
-    for i in range (0, n):
-        print(x[i])
-    # step 7
-    # THIS PART IS NOT WORKING AS EXPECTED  
+    print(f"random sequence: {x}")
+    # step 7 
     x1 = x.copy()
     x1.sort()
     if (n + 1) % 2 == 0: 
-        median = (x1[(n + 1)/2] + x1[((n + 1)/ 2) + 1]) / 2
-    elif (n + 1) % 2 != 0: 
-        median = x1[((n + 1) + 1)/ 2]
+        median = (x1[(n + 1)/2] + x1[((n + 1)/ 2) + 1]) / 2 # INDEX IS NOT INT
+    elif (n + 1) % 2 == 1: 
+        median = x1[((n + 1) + 1)/ 2] # INDEX IS NOT INT
     # step 8 & 9
     lu = []
     for i in x: 
@@ -72,12 +85,3 @@ def kotta():
     print(absZ)
 
 kotta()
-    
-
-        
-
-
-    
-
-
-
